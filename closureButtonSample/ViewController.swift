81f45e01c6384bfbb88ae5460b9ebd25164fc354
ha1fha1f <half.ldk.hiz@gmail.com>
2015-12-01 13:18:59 +0900

    Initial Commit

---
+++ closureButtonSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  closureButtonSample
+//
+//  Created by 山口智生 on 2015/12/01.
+//  Copyright © 2015年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
