a1c82213e950e0f0394e07d5680157837e5b65e9
ha1fha1f <half.ldk.hiz@gmail.com>
2015-12-01 17:33:59 +0900

    動作確認

--- closureButtonSample/ViewController.swift
+++ closureButtonSample/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
+class ClosureButton: UIButton {
+    
+    private var touchUpMethod: ((ClosureButton) -> Void)?
+    
+    func dispatchOnTouchUpInside(closure: (ClosureButton) -> Void) {
+        self.touchUpMethod = closure
+        self.addTarget(self, action: "onTouchUpInside:", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    func onTouchUpInside(button: ClosureButton!) {
+        touchUpMethod?(button)
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let button = ClosureButton(frame: CGRectMake(0, 0, 100, 40))
+        button.layer.position = self.view.center
+        button.backgroundColor = UIColor.blackColor()
+        button.dispatchOnTouchUpInside() { button in
+            print("touch")
+        }
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
